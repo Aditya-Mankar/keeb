715bb304b641b1f516fa40040b55cb27758ce3cc
Aditya Mankar <51234261+Aditya-Mankar@users.noreply.github.com>
2022-07-27 12:17:09 +0530

    Implemented config server

--- notification-service/src/main/java/com/keeb/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/keeb/notificationservice/NotificationServiceApplication.java
@@ -2,12 +2,14 @@ package com.keeb.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@RefreshScope
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@RefreshScope
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
